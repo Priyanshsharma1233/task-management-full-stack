b6c0297b6654a94599950a1e5ea0536c44abba85
Priyansh Sharma <priyanshsharma1233@gmail.com>
2026-08-19 21:52:43 +0530

    Prepare backend for production deployment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -16,8 +16,7 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT ?? 3001);
-
+  await app.listen(process.env.PORT || 3001);
   console.log('Backend running at http://localhost:3001');
 }
 
